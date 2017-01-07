7f7289621c584b7e6e603cf4e56b6713c21445e9
DAROT <darotudeen@gmail.com>
2017-01-07 02:41:55 -0800

    commiting fizzbuzz

---
+++ app/fizzbuzz.js
@@ -0,0 +1,16 @@
+'use strict'
+var file = {}
+file.fizzBuzz = function (num){
+	if(num % 3 !== 0 && num%5 !== 0){
+		return number
+	}
+	else if (num%3 === 0 && num%5===0){
+		return "FizzBuzz"
+	}
+	else if (num%3 ==0){
+		return "Fizz"
+	}
+	else if (num%5===0){
+		return "Buzz"
+	}
+}
